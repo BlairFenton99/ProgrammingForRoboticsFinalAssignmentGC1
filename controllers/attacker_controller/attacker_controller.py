eea58058b65e66b535691e98cebc9c7b021bb370
Oscar-ICT <oscar.levi.chia@gmail.com>
2026-05-04 17:15:17 +1000

    feature: added vscode intergration

--- controllers/attacker_controller/attacker_controller.py
+++ controllers/attacker_controller/attacker_controller.py
@@ -3,15 +3,16 @@ from controller import Robot
 robot = Robot()
 timestep = int(robot.getBasicTimeStep())
 
-# Motors
 left_motor = robot.getDevice('left wheel motor')
 right_motor = robot.getDevice('right wheel motor')
 
 left_motor.setPosition(float('inf'))
 right_motor.setPosition(float('inf'))
 
-left_motor.setVelocity(3.0)
-right_motor.setVelocity(3.0)
+SPEED = 6.28
+
+left_motor.setVelocity(SPEED)
+right_motor.setVelocity(SPEED)
 
 while robot.step(timestep) != -1:
     pass
